27539cc48c000bd5a33a5f28362d04eddbef1f95
Mamaev Semyon <avot.semen@gmail.com>
2024-05-09 23:19:44 +0500

    Добавил фукцию создания нового массива из строк, где длина строки <=3

--- Task/Program.cs
+++ Task/Program.cs
@@ -17,12 +17,15 @@ class Program
     {
         // Устанавливаем кодировку ввода на UTF-16 чтобы работало с кирилицей
         Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");
-        
+
         System.Console.WriteLine("Введите строку из слов, разделенных пробелами:");
         string input = Console.ReadLine(); // Получаем ввод от пользователя
         string[] originalArray = input.Split(' '); // Разбиваем введенную строку на элементы массива по пробелам и сохраняем в массив
+        string[] newArray = CreateArrayMaxThreeChars(originalArray); // Создаем новый массив с помощью функции
 
-        PrintArray(originalArray);
+        PrintArray(originalArray); // Печать оригинального массива
+        PrintArray(newArray); // Печать нового массива
+
 
         //
         // Функции
@@ -35,6 +38,24 @@ class Program
                 System.Console.Write(array[i] + " ");
             }
             System.Console.WriteLine();
+        }
+
+        string[] CreateArrayMaxThreeChars(string[] array) //Функция создания нового массива где элементы не длиннее 3 символов
+        {
+            string[] newArray = new string[originalArray.Length]; // Создаем новый массив такой же длины, как оригинальный
+            int newIndex = 0; // Инициализируем индекс для нового массива
+
+            // Формирование нового массива
+            foreach (string str in originalArray) // Перебираем каждый элемент оригинального массива
+            {
+                // Если длина строки <= 3 символов, добавляем в новый массив
+                if (str.Length <= 3) // Проверяем длину текущей строки
+                {
+                    newArray[newIndex] = str; // Добавляем строку в новый массив
+                    newIndex++; // Увеличиваем индекс нового массива для следующего элемента
+                }
+            }
+            return newArray; //возвращаем новый массив
         }
     }
 
